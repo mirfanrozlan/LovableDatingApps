422fc0e4e1bd837650a1e7153d9e86537e3ee889
mirfanrozlan <muhammadirfanrozlan@gmail.com>
2025-12-24 11:13:06 +0800

    Redesign Bottom Nav

--- lib/widgets/messages/app_bottom_nav.dart
+++ lib/widgets/messages/app_bottom_nav.dart
@@ -8,73 +8,98 @@ class AppBottomNav extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return NavigationBar(
-      selectedIndex: currentIndex,
-      onDestinationSelected: (i) {
-        if (i == 0) {
-          Navigator.pushNamedAndRemoveUntil(
-            context,
-            AppRoutes.discover,
-            (route) => false,
-          );
-        } else if (i == 1) {
-          Navigator.pushNamedAndRemoveUntil(
-            context,
-            AppRoutes.messages,
-            (route) => false,
-          );
-        } else if (i == 2) {
-          Navigator.pushNamedAndRemoveUntil(
-            context,
-            AppRoutes.moments,
-            (route) => false,
-          );
-        } else if (i == 3) {
-          Navigator.pushNamedAndRemoveUntil(
-            context,
-            AppRoutes.friends,
-            (route) => false,
-          );
-        } else if (i == 4) {
-          Navigator.pushNamedAndRemoveUntil(
-            context,
-            AppRoutes.me,
-            (route) => false,
-          );
-        } else {
-          ScaffoldMessenger.of(
-            context,
-          ).showSnackBar(const SnackBar(content: Text('Coming soon')));
-        }
-      },
-      indicatorColor: AppTheme.primary.withValues(alpha: 0.15),
-      destinations: const [
-        NavigationDestination(
-          icon: Icon(Icons.explore_outlined),
-          selectedIcon: Icon(Icons.explore),
-          label: 'Discover',
+    return NavigationBarTheme(
+      data: NavigationBarThemeData(
+        labelTextStyle: MaterialStateProperty.resolveWith<TextStyle>(
+          (states) {
+            if (states.contains(MaterialState.selected)) {
+              return const TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.w600,
+                fontSize: 11,
+              );
+            }
+            return const TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w500,
+              fontSize: 11,
+            );
+          },
         ),
-        NavigationDestination(
-          icon: Icon(Icons.chat_bubble_outline),
-          selectedIcon: Icon(Icons.chat_bubble),
-          label: 'Messages',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.image_outlined),
-          selectedIcon: Icon(Icons.image),
-          label: 'Moments',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.group_outlined),
-          selectedIcon: Icon(Icons.group),
-          label: 'Friends',
-        ),
-        NavigationDestination(
-          icon: Icon(Icons.person_outline),
-          selectedIcon: Icon(Icons.person),
-          label: 'Me',
-        ),
-      ],
+      ),
+      child: NavigationBar(
+        height: 64,
+        backgroundColor: Color(0xFFF3F4F6),
+        surfaceTintColor: Colors.transparent,
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysShow,
+        indicatorColor: Colors.transparent,
+        indicatorShape: const RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+        selectedIndex: currentIndex,
+        onDestinationSelected: (i) {
+          if (i == 0) {
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              AppRoutes.discover,
+              (route) => false,
+            );
+          } else if (i == 1) {
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              AppRoutes.messages,
+              (route) => false,
+            );
+          } else if (i == 2) {
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              AppRoutes.moments,
+              (route) => false,
+            );
+          } else if (i == 3) {
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              AppRoutes.friends,
+              (route) => false,
+            );
+          } else if (i == 4) {
+            Navigator.pushNamedAndRemoveUntil(
+              context,
+              AppRoutes.me,
+              (route) => false,
+            );
+          } else {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('Coming soon')),
+            );
+          }
+        },
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.explore_outlined),
+            selectedIcon: Icon(Icons.explore),
+            label: 'Discover',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.chat_bubble_outline),
+            selectedIcon: Icon(Icons.chat_bubble),
+            label: 'Messages',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.image_outlined),
+            selectedIcon: Icon(Icons.image),
+            label: 'Moments',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.favorite_border),
+            selectedIcon: Icon(Icons.favorite),
+            label: 'Matches',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person_outline),
+            selectedIcon: Icon(Icons.person),
+            label: 'Me',
+          ),
+        ],
+      ),
     );
   }
 }
